aebf1a6eb10eebf5c3ea4c1026a6ba767fa2b756
SuhailMahmood <suhailmahmood@outlook.com>
2015-07-13 23:31:46 +0600

    Gene has a method to set the value of the Gene.
    This method is used in the Chromosome.java in setAllele() method, to set
    the value of a target Gene in a target Chromosome. This solves a major
    problem in the whole project! Also get rid of the constructor that takes
    an int as parameter to initialize the Gene as that is not needed.

--- src/ga/Gene.java
+++ src/ga/Gene.java
@@ -2,14 +2,14 @@ package ga;
 
 /**
  * This is a single gene, which actually wraps a bit. Provides features to
- * manipulate the gene
+ * manipulate the gene.
  *
  * @author Suhail
  */
 public class Gene {
 
     /**
-     * the value of the gene, either 0 or 1
+     * the value of the gene, either 0 or 1.
      */
     private int value;
 
@@ -17,30 +17,7 @@ public class Gene {
      * Default constructor. Creates a gene with either a 0 or a 1, set randomly.
      */
     public Gene() {
-        value = Math.random() <= 0.5 ? 0 : 1;
-    }
-
-    /**
-     * Constructor with value of the gene as parameter. If the argument is
-     * anything other than a 0 or a 1, IllegalArgumentException is thrown.
-     *
-     * @param value the value of the gene to set to.
-     */
-    public Gene(int value) {
-        if (value != 0 && value != 1) {
-            throw new IllegalArgumentException("value must be either 0 or 1");
-        }
-        else {
-            this.value = value;
-        }
-    }
-
-    /**
-     * Flips the current gene value. A 0 is flipped to a 1, and a 1 is flipped
-     * to 0
-     */
-    public void mutate() {
-        value = 1 - value;
+        value = Math.random() < 0.5 ? 0 : 1;
     }
 
     /**
@@ -48,8 +25,29 @@ public class Gene {
      *
      * @return the current gene value, either 0 or 1.
      */
-    public int value() {
+    public int getValue() {
         return value;
+    }
+
+    /**
+     * Set the value of the gene. IllegalArgumentException is thrown is the
+     * argument is anything other than a 0 or a 1.
+     *
+     * @param value the value to set the gene to.
+     */
+    public void setValue(int value) {
+        if (value != 0 && value != 1) {
+            throw new IllegalArgumentException("value must be either 0 or 1");
+        }
+        this.value = value;
+    }
+
+    /**
+     * Flips the current gene value. A 0 is flipped to a 1, and a 1 is flipped
+     * to 0.
+     */
+    public void mutate() {
+        value = 1 - value;
     }
 
     /**
